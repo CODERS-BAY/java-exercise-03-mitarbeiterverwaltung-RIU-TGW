d5aaa9ab82a0a077de86b4c56fdd99eda7627574
riu <riu@tgw.at>
2021-12-09 17:27:31 +0100

    submission

--- src/app/src/main/java/application/Department.java
+++ src/app/src/main/java/application/Department.java
@@ -1,6 +1,25 @@
 package application;
 
 public class Department {
+	// fields
+	private String name;
+	private int id;
+	private String counter;
+	private String city;
+
+	// Constructors
+
+	public Department() {
+
+	}
+
+	public Department(String name, int id, String counter, String city) {
+		this.name = name;
+		this.id = id;
+		this.counter = counter;
+		this.city = city;
+	}
+
 	public boolean isEquals(Department other) {
 		return false;
 	}
